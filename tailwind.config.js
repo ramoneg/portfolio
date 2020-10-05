a6fe09fc38450db14134e5a747dadb0c1699cf34
Ramon Egger <egger@racerfish.com>
2020-10-05 10:40:51 +0200

    Custom color theme, font, future settings

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'ocean': {
+          '50': '#F3F4F8',
+          '100': '#E6EAF0',
+          '200': '#C2CADA',
+          '300': '#9DAAC3',
+          '400': '#536A96',
+          '500': '#092A69',
+          '600': '#08265F',
+          '700': '#05193F',
+          '800': '#04132F',
+          '900': '#030D20',
+          },
+      },
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
