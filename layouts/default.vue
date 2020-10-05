e794bb306f91efb276204c7500db2c0b290a7a38
Ramon Egger <egger@racerfish.com>
2020-10-05 10:41:09 +0200

    default page layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-8 sm:p-10 md:p-16 flex justify-center">
+    <div class="bg-ocean-500 w-full sm:w-3/4 xl:w-1/2 max-w-3xl" ref="content">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+      // Set Meta Tags for this Page
+    },
+}
+</script>
