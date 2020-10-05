8ef6cc91fe5a1c204c20da9fc1c0e25315bc1495
Ramon Egger <egger@racerfish.com>
2020-10-05 16:34:23 +0200

    Redirect on error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
-  </div>
+  <div></div>
 </template>
 
 <script>
   export default {
     props: ['error'],
+    created () {
+      this.$router.push('/')
+    }
   }
 </script>
