d568ba03a4fe92577eff5b06c7e56bdc17c1b1b0
Ramon Egger <egger@racerfish.com>
2020-10-05 16:02:53 +0200

    Refactor

--- components/page/h2.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h2 class="title text-4xl font-bold"><slot /></h2>
-</template>
-
-<script>
-export default {
-    name: 'h2'
-}
-</script>
---
+++ components/page/pageTitle.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2 class="text-4xl font-bold"><slot /></h2>
+</template>
+
+<script>
+export default {
+    name: 'pageTitle'
+}
+</script>
